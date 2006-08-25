a15fd42b45373d0735f78b29215f836c77e54329
Adrian Georgescu <ag@ag-projects.com>
2006-08-25 10:28:10 +0000

    Update documentation
    Added a sample apache2 virtual host example

--- setup/mysql/setup_mysql.sh
+++ setup/mysql/setup_mysql.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 db=cdrtool
-mysqladmin 	-u root -p$1 -h $2 drop $db
+mysql 	-u root -p$1 -h $2 -e "drop database if exists $db"
 mysqladmin 	-u root -p$1 -h $2 create $db
 mysql 		-u root -p$1 -h $2 mysql < ./create_users.mysql
 mysql 		-u root -p$1 -h $2 $db   < ./create_tables.mysql
 mysql 		-u root -p$1 -h $2 $db   < ./create_data.mysql
-mysqladmin 	-u root -p$1 -h $2 reload
