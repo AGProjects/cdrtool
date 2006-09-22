9921250ef08f102a5c23193d3c1a27346d68c5c4
Adrian Georgescu <ag@ag-projects.com>
2006-09-22 11:25:36 +0000

    Added cron job for statistics and sample configuration entry

--- debian/cron.d
+++ debian/cron.d
@@ -4,3 +4,5 @@
   0  2 1 * * root php /var/www/CDRTool/scripts/SER/quotaReset.php >/dev/null 
   0  3 1 * * root php /var/www/CDRTool/scripts/SER/rotateTables.php >/dev/null 
   0  4 * * * root php /var/www/CDRTool/scripts/purgeTables.php >/dev/null
+*/5  * * * * root php /var/www/CDRTool/scripts/buildStatistics.php >/dev/null
+  
