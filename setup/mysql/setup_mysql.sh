266ae819f77ac1aa037ee73614eac72969316beb
Adrian Georgescu <ag@ag-projects.com>
2006-10-22 10:45:38 +0000

    Add checks to mysql creation script

--- setup/mysql/setup_mysql.sh
+++ setup/mysql/setup_mysql.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
+
 db=cdrtool
-mysql 	-u root -p$1 -h $2 -e "drop database if exists $db"
+
+if test $# != 2; then
+	echo "setup_mysql.sh DB_ROOT_PASSWORD DB_HOST"
+	exit;
+fi
+
+grep \'PASSWORD\' create_users.mysql | grep -i grant > /dev/null
+
+if test $? = 0  ; then
+    echo "Please edit create_users.mysql and replace the PASSWORD with a new password and try again"
+    exit 1
+fi
+
+grep \'PRIVATE_IP_NETWORK\' create_users.mysql | grep -i grant > /dev/null
+
+if test $? = 0  ; then
+    echo "Please edit create_users.mysql and replace the PRIVATE_IP_NETWORK with real addresses and try again"
+    exit 1
+fi
+
 mysqladmin 	-u root -p$1 -h $2 create $db
-mysql 		-u root -p$1 -h $2 mysql < ./create_users.mysql
-mysql 		-u root -p$1 -h $2 $db   < ./create_tables.mysql
-mysql 		-u root -p$1 -h $2 $db   < ./create_data.mysql
+
+if test $? = 0  ; then
+	mysql 		-u root -p$1 -h $2 mysql < ./create_users.mysql
+	mysql 		-u root -p$1 -h $2 $db   < ./create_tables.mysql
+	mysql 		-u root -p$1 -h $2 $db   < ./create_data.mysql
+else 
+	echo "Failed to create $db database on host $2"
+	exit 1
+fi
+
+exit 0
