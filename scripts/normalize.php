a148405df1c7024ef7505f259c7b082b975a0156
Adrian Georgescu <ag@ag-projects.com>
2006-12-13 11:45:35 +0000

    Fixed logging of normalization
    Removed normalize0SecCalls, is useless

--- scripts/normalize.php
+++ scripts/normalize.php
@@ -1,14 +1,13 @@
 #!/usr/bin/php
 <?
 set_time_limit(0);
+
 $path=dirname(realpath($_SERVER['PHP_SELF']));
 include($path."/../global.inc");
 include($path."/../cdrlib.phtml");
 
 define_syslog_variables();
-
 openlog("CDRTool", LOG_PID, LOG_LOCAL0);
-//$verbose=1 ;
 
 $lockFile="/var/lock/CDRTool_normalize.lock";
 
@@ -29,32 +28,50 @@ while (list($k,$v) = each($DATASOURCES)) {
     if (strlen($v["normalizedField"])) {
         $b=time();
         $class_name=$v["class"];
-        //printf ("CDR class: %s: %s",$class_name,$v["normalizedField"]);
 
         unset($CDRS);
+
         $CDRS = new $class_name($k);
-        $total   = 0;
-        $success = 0;
-        $failure = 0;
-        $$speed  = 0;
 
-        $results=$CDRS->NormalizeCDRS();
+        $log=sprintf("Normalize datasource %s, database %s, table %s\n",$k,$v['db_class'],$CDRS->table);
+        print $log;
+        syslog(LOG_NOTICE,$log);
 
-        if ($results[total])   $total   =$results[total];
-        if ($results[success]) $success =$results[success];
-        if ($results[failure]) $success =$results[failure];
+        $CDRS->NormalizeCDRS();
 
         $e=time();
         $d=$e-$b;
 
-        if ($d > 0) {
-            $speed=number_format($results[total]/$d,0,"","");
-            $speed=number_format($total/$d,0,"","");
+        if ($CDRS->status['cdr_to_normalize']) {
+            $speed=0;
+            if ($d) $speed=number_format($CDRS->status['cdr_to_normalize']/$d,0,"","");
+        	$log=sprintf("%d CDRs, %d normalized in %s s @ %s cps\n",$CDRS->status['cdr_to_normalize'],$CDRS->status['normalized'],$d,$speed);
+            print $log;
+        	syslog(LOG_NOTICE,$log);
         }
 
-        if ($total) {
-        	print "$k: $total CDRS $success normalized in $d s @ $speed cps\n";
-        	syslog(LOG_NOTICE,"$k: $total CDRs, $success normalized in $d s @ $speed cps");
+        if (preg_match("/^(\w+)\d{6}$/",$CDRS->table,$m)) {
+        	$lastMonthTable=$m[1].date('Ym', mktime(0, 0, 0, date("m")-1, "01", date("Y")));
+
+            $log=sprintf("Normalize datasource %s, database %s, table %s\n",$k,$v['db_class'],$lastMonthTable);
+            print $log;
+            syslog(LOG_NOTICE,$log);
+
+	        $b=time();
+
+			$CDRS->table=$lastMonthTable;
+            $CDRS->NormalizeCDRS();
+
+            if ($CDRS->status['cdr_to_normalize']) {
+                $e=time();
+                $d=$e-$b;
+
+                $speed=0;
+                if ($d) $speed=number_format($CDRS->status['cdr_to_normalize']/$d,0,"","");
+                $log=sprintf("%d CDRs, %d normalized in %s s @ %s cps\n",$CDRS->status['cdr_to_normalize'],$CDRS->status['normalized'],$d,$speed);
+                print $log;
+                syslog(LOG_NOTICE,$log);
+            }
         }
     }
 }
