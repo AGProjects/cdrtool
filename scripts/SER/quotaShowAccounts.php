7e3cf934e391607f50151c5731a7ee0c612ec66e
Adrian Georgescu <ag@ag-projects.com>
2007-01-03 10:42:01 +0000

    * Added documentation for the Quota system (doc/QuotaSystem.txt)

--- scripts/SER/quotaShowAccounts.php
+++ scripts/SER/quotaShowAccounts.php
@@ -4,6 +4,17 @@ $path=dirname(realpath($_SERVER['PHP_SELF']));
 include($path."/../../global.inc");
 include($path."/../../cdrlib.phtml");
 
+if (!count($argv) || count($argv) != 2) {
+    print "Syntax: $PHP_SELF treshhold\n";
+    print "Accounts with quota usage percentage greater than the treshhold will be showed.\n";
+    return 0;
+}
+
+if (!preg_match("/^\d{1,2}$/",$argv[1])) {
+    print "Error: treshold must be an integer between 0 and 100\n";
+    return 0;
+}
+
 $cdr_source     = "ser_radius";
 $CDR_class      = $DATASOURCES[$cdr_source]["class"];
 $CDRS           = new $CDR_class($cdr_source);
@@ -14,6 +25,7 @@ if (!$SERQuota_class) $SERQuota_class="SERQuota";
 
 $Quota = new $SERQuota_class($CDRS);
 
-$Quota->showAccountsWithQuota();
+
+$Quota->showAccountsWithQuota($argv[1]);
 
 ?>
