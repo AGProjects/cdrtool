6ffb93c47ebf142320a22a78f46d77fdb31a652f
Adrian Georgescu <ag@ag-projects.com>
2007-09-03 10:15:38 +0000

    Added changelog for version 5.2.5:
      * Replaced multiple SOAP definitions from global.inc with a pointer
        to and entry from the $soapEngines defined in soap_engines.inc            
      * Fixed read of HTTP vars in sip trace, public traces are now visible 
        when gloabl vars are turned off
    
    More work on provisioning
    Fixed broken statististics

--- scripts/buildStatistics.php
+++ scripts/buildStatistics.php
@@ -4,6 +4,10 @@ $path=dirname(realpath($_SERVER['PHP_SELF']));
 include($path."/../global.inc");
 include($path."/../sip_statistics_lib.phtml");
 
+$errorReporting = (E_ALL & ~(E_NOTICE | E_WARNING));
+//$errorReporting = 1; // temporary until warnings are fixed
+error_reporting($errorReporting);
+
 $SIPstatistics=new SIPstatistics ();
 $SIPstatistics->buildStatistics();
 
--- scripts/harvestStatistics.php
+++ scripts/harvestStatistics.php
@@ -5,6 +5,10 @@ $path=dirname(realpath($_SERVER['PHP_SELF']));
 include($path."/../global.inc");
 include($path."/../sip_statistics_lib.phtml");
 
+$errorReporting = (E_ALL & ~(E_NOTICE | E_WARNING));
+//$errorReporting = 1; // temporary until warnings are fixed
+error_reporting($errorReporting);
+
 $SIPstatistics=new SIPstatistics ();
 $SIPstatistics->harvestStatistics();
 
