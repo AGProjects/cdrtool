45a61a8e1215b5564045ddb6f0ede9fd2041c201
Adrian Georgescu <ag@ag-projects.com>
2008-01-05 21:51:46 +0000

    Make execution time infinite

--- scripts/importRatingTables.php
+++ scripts/importRatingTables.php
@@ -1,21 +1,15 @@
 #!/usr/bin/php
 <?
-
 $path=dirname(realpath($_SERVER['PHP_SELF']));
 include($path."/../global.inc");
 include($path."/../rating_lib.phtml");
 
-Header("Content-type: text/plain");
-Header("Content-Disposition: inline; filename=Rating.txt");
-header("Cache-Control: no-cache, must-revalidate");  // HTTP/1.1
-header("Pragma: no-cache");
-
-ini_set('max_execution_time','36000');
+set_time_limit(0);
 
 $RatingTables= new RatingTables();
 $RatingTables->ImportCSVFiles();
 
 if ($RatingTables->mustReload && !reloadRatingEngineTables()) {
-	print "Error: cannot connect to network rating engine\n";
+    print "Error: cannot connect to network rating engine\n";
 }
 ?>
