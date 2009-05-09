612f875b167e122d8329abd53bbd6c195605b84f
Adrian Georgescu <ag@ag-projects.com>
2009-05-09 11:50:11 +0000

    Added lock for import script

--- scripts/importRatingTables.php
+++ scripts/importRatingTables.php
@@ -5,10 +5,28 @@ require("rating.php");
 
 set_time_limit(0);
 
+$lockFile=sprintf("/var/lock/CDRTool_import_rates.lock");
+
+$abort_text="Another import operation is in progress. Try again later.\n";
+
+$f=fopen($lockFile,"w");
+if (flock($f, LOCK_EX + LOCK_NB, $w)) {
+    if ($w) {
+        print $abort_text;
+        syslog(LOG_NOTICE,$abort_text);
+        exit(2);
+    }
+} else {
+    print $abort_text;
+    syslog(LOG_NOTICE,$abort_text);
+    exit(1);
+}
+
 $RatingTables= new RatingTables();
 $RatingTables->ImportCSVFiles();
 
 if ($RatingTables->mustReload && !reloadRatingEngineTables()) {
     print "Error: cannot connect to network rating engine\n";
 }
+
 ?>
