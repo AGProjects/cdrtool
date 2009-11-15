28dfa52236c4fe6aa782e1f0749dc1ddc4b9736b
Adrian Georgescu <ag@ag-projects.com>
2009-11-15 13:41:27 +0000

    Added initial translation files

---
+++ po/merge.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+make cdrtool.pot
+msgmerge ro.po cdrtool.pot > ronew.po; mv ronew.po ro.po
+msgmerge nl.po cdrtool.pot > nlnew.po; mv nlnew.po nl.po
+msgmerge de.po cdrtool.pot > denew.po; mv denew.po de.po
+msgmerge es.po cdrtool.pot > esnew.po; mv esnew.po es.po
