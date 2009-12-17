78d958ed77ffff1690bce6ba1ddd51b27d7045e4
Adrian Georgescu <ag@ag-projects.com>
2009-12-17 13:52:04 +0000

    Fixed name of Arris_TM722b image

--- library/phone_images.php
+++ library/phone_images.php
@@ -41,7 +41,7 @@ $userAgentImages = array("Asterisk"       => "asterisk.png",
                          "sipsimple"      => "blink.png",
                          "blink"          => "blink.png",
                          "fring"          => "fring.png",
-                         "ARRIS-TM722"    => "Arris_TM722b.jpg",
+                         "ARRIS-TM722"    => "Arris_TM722b.png",
                          "Genexis OCG118" => "genexis.png"
                          );
 
