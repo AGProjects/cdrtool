43e42e7faf640682b141ec30796fd76d27564b32
Adrian Georgescu <ag@ag-projects.com>
2010-04-22 16:08:00 +0000

    Update blink logo

--- library/sip_settings.php
+++ library/sip_settings.php
@@ -73,7 +73,7 @@ class SipSettings {
     var $call_img                  = "<img src=images/call.gif border=0 alt='Dial'>";
     var $delete_img                = "<img src=images/del_pb.gif border=0 alt='Delete'>";
     var $plus_sign_img             = "<img src=images/plus_sign.png border=0 alt='Add Contact'>";
-    var $embedded_img              = " <img src=images/blink.png border=0>";
+    var $embedded_img              = "<img src=images/blink.png border=0>";
 
     var $groups                    = array();
 
