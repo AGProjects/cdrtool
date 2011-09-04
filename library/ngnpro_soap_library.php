13ff74564e5c2fa8e18f51dc6090ef7b8a37d36c
Adrian Georgescu <ag@ag-projects.com>
2011-09-04 09:47:12 +0000

    Fixed initializing soap engine

--- library/ngnpro_soap_library.php
+++ library/ngnpro_soap_library.php
@@ -1549,63 +1549,93 @@ class WebService_NGNPro_NetworkPort extends SOAP_Client_Custom
 
 class WebService_SoapSIMPLEProxy_PresencePort extends SOAP_Client_Custom
 {
-    function WebService_SoapSIMPLEProxy_PresencePort($url)
-{
-        $this->SOAP_Client_Custom($url, 0);
+    function WebService_SoapSIMPLEProxy_PresencePort($path)
+    {
+        $this->SOAP_Client($path, 0);
     }
-    function &setPresenceInformation($sipId, $password, $information) {
+    function &setPresenceInformation($sipId, $password, $information)
+    {
+        // sipId is a ComplexType SipId,
+        // refer to wsdl for more info
+        $sipId = new SOAP_Value('sipId', '{urn:AGProjects:SoapSIMPLEProxy}SipId', $sipId);
         // information is a ComplexType PresenceInformation,
-        //refer to wsdl for more info
-        $information =new SOAP_Value('information','{urn:AGProjects:SoapSIMPLEProxy}PresenceInformation',$information);
-        return $this->call("setPresenceInformation", 
-                        $v = array("sipId"=>$sipId, "password"=>$password, "information"=>$information), 
-                        array('namespace'=>'urn:AGProjects:SoapSIMPLEProxy:Presence',
-                            'soapaction'=>'',
-                            'style'=>'rpc',
-                            'use'=>'encoded' ));
+        // refer to wsdl for more info
+        $information = new SOAP_Value('information', '{urn:AGProjects:SoapSIMPLEProxy}PresenceInformation', $information);
+        $result = $this->call('setPresenceInformation',
+                              $v = array('sipId' => $sipId, 'password' => $password, 'information' => $information),
+                              array('namespace' => 'urn:AGProjects:SoapSIMPLEProxy:Presence',
+                                    'soapaction' => '',
+                                    'style' => 'rpc',
+                                    'use' => 'encoded'));
+        return $result;
     }
-    function &getPresenceInformation($sipId, $password) {
-        return $this->call("getPresenceInformation", 
-                        $v = array("sipId"=>$sipId, "password"=>$password), 
-                        array('namespace'=>'urn:AGProjects:SoapSIMPLEProxy:Presence',
-                            'soapaction'=>'',
-                            'style'=>'rpc',
-                            'use'=>'encoded' ));
+    function &getPresenceInformation($sipId, $password)
+    {
+        // sipId is a ComplexType SipId,
+        // refer to wsdl for more info
+        $sipId = new SOAP_Value('sipId', '{urn:AGProjects:SoapSIMPLEProxy}SipId', $sipId);
+        $result = $this->call('getPresenceInformation',
+                              $v = array('sipId' => $sipId, 'password' => $password),
+                              array('namespace' => 'urn:AGProjects:SoapSIMPLEProxy:Presence',
+                                    'soapaction' => '',
+                                    'style' => 'rpc',
+                                    'use' => 'encoded'));
+        return $result;
     }
-    function &deletePresenceInformation($sipId, $password) {
-        return $this->call("deletePresenceInformation", 
-                        $v = array("sipId"=>$sipId, "password"=>$password), 
-                        array('namespace'=>'urn:AGProjects:SoapSIMPLEProxy:Presence',
-                            'soapaction'=>'',
-                            'style'=>'rpc',
-                            'use'=>'encoded' ));
+    function &deletePresenceInformation($sipId, $password)
+    {
+        // sipId is a ComplexType SipId,
+        // refer to wsdl for more info
+        $sipId = new SOAP_Value('sipId', '{urn:AGProjects:SoapSIMPLEProxy}SipId', $sipId);
+        $result = $this->call('deletePresenceInformation',
+                              $v = array('sipId' => $sipId, 'password' => $password),
+                              array('namespace' => 'urn:AGProjects:SoapSIMPLEProxy:Presence',
+                                    'soapaction' => '',
+                                    'style' => 'rpc',
+                                    'use' => 'encoded'));
+        return $result;
     }
-    function &getWatchers($sipId, $password) {
-        return $this->call("getWatchers", 
-                        $v = array("sipId"=>$sipId, "password"=>$password), 
-                        array('namespace'=>'urn:AGProjects:SoapSIMPLEProxy:Presence',
-                            'soapaction'=>'',
-                            'style'=>'rpc',
-                            'use'=>'encoded' ));
+    function &getWatchers($sipId, $password)
+    {
+        // sipId is a ComplexType SipId,
+        // refer to wsdl for more info
+        $sipId = new SOAP_Value('sipId', '{urn:AGProjects:SoapSIMPLEProxy}SipId', $sipId);
+        $result = $this->call('getWatchers',
+                              $v = array('sipId' => $sipId, 'password' => $password),
+                              array('namespace' => 'urn:AGProjects:SoapSIMPLEProxy:Presence',
+                                    'soapaction' => '',
+                                    'style' => 'rpc',
+                                    'use' => 'encoded'));
+        return $result;
     }
-    function &setPolicy($sipId, $password, $policy) {
+    function &setPolicy($sipId, $password, $policy)
+    {
+        // sipId is a ComplexType SipId,
+        // refer to wsdl for more info
+        $sipId = new SOAP_Value('sipId', '{urn:AGProjects:SoapSIMPLEProxy}SipId', $sipId);
         // policy is a ComplexType PresencePolicy,
-        //refer to wsdl for more info
-        $policy =new SOAP_Value('policy','{urn:AGProjects:SoapSIMPLEProxy}PresencePolicy',$policy);
-        return $this->call("setPolicy", 
-                        $v = array("sipId"=>$sipId, "password"=>$password, "policy"=>$policy), 
-                        array('namespace'=>'urn:AGProjects:SoapSIMPLEProxy:Presence',
-                            'soapaction'=>'',
-                            'style'=>'rpc',
-                            'use'=>'encoded' ));
+        // refer to wsdl for more info
+        $policy = new SOAP_Value('policy', '{urn:AGProjects:SoapSIMPLEProxy}PresencePolicy', $policy);
+        $result = $this->call('setPolicy',
+                              $v = array('sipId' => $sipId, 'password' => $password, 'policy' => $policy),
+                              array('namespace' => 'urn:AGProjects:SoapSIMPLEProxy:Presence',
+                                    'soapaction' => '',
+                                    'style' => 'rpc',
+                                    'use' => 'encoded'));
+        return $result;
     }
-    function &getPolicy($sipId, $password) {
-        return $this->call("getPolicy", 
-                        $v = array("sipId"=>$sipId, "password"=>$password), 
-                        array('namespace'=>'urn:AGProjects:SoapSIMPLEProxy:Presence',
-                            'soapaction'=>'',
-                            'style'=>'rpc',
-                            'use'=>'encoded' ));
+    function &getPolicy($sipId, $password)
+    {
+        // sipId is a ComplexType SipId,
+        // refer to wsdl for more info
+        $sipId = new SOAP_Value('sipId', '{urn:AGProjects:SoapSIMPLEProxy}SipId', $sipId);
+        $result = $this->call('getPolicy',
+                              $v = array('sipId' => $sipId, 'password' => $password),
+                              array('namespace' => 'urn:AGProjects:SoapSIMPLEProxy:Presence',
+                                    'soapaction' => '',
+                                    'style' => 'rpc',
+                                    'use' => 'encoded'));
+        return $result;
     }
 }
 ?>
