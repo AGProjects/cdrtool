f69d9646c17d2c4a15881a2386039dce3cd391ca
Tijmen de Mes <tijmen@ag-projects.com>
2012-01-16 16:24:49 +0000

    Added an extra MaxRate scenario

--- library/cdr_generic.php
+++ library/cdr_generic.php
@@ -2941,10 +2941,10 @@ class MaxRate extends CSVWritter {
                 $cdr['origin'] = "+".$m[1];
             } else if (preg_match("/^([1-9][0-9]+)@(.*)$/",$CDR->aNumberPrint,$m)) {
                 $cdr['origin'] = "+31".$m[1];
-            } else if (preg_match("/^\+0+)@(.*)$/",$CDR->aNumberPrint,$m)) {
-                $cdr['origin'] = "+31123456789";
             } else if (preg_match("/^(\+[1-9][0-9]+)@(.*)$/",$CDR->aNumberPrint,$m)) {
                 $cdr['origin'] = $m[1];
+            } else if (preg_match("/^\+?0[0-9]?+@?(.*)?$/",$CDR->aNumberPrint,$m)) {
+                $cdr['origin'] = "+31123456789";
             } else if (preg_match("/^ims.imscore.net.*$/",$CDR->aNumberPrint,$m)) {
                 $cdr['origin'] = "+31123456789";
             } else if (preg_match("/^anonymous@(.*)$/",$CDR->aNumberPrint) && $CDR->SipRPID) {
@@ -2956,7 +2956,7 @@ class MaxRate extends CSVWritter {
                     $cdr['origin'] = "+31".$m[1];
                 } else if (preg_match("/^(\+[1-9][0-9]+)@(.*)$/",$CDR->SipRPID,$m)) {
                    $cdr['origin'] = $m[1];
-                } else if (preg_match("/^\+0+)@(.*)$/",$CDR->SipRPID,$m)) {
+                } else if (preg_match("/^\+?0[0-9]?+@?(.*)?$/",$CDR->SipRPID,$m)) {
                    $cdr['origin'] = "+31123456789";
                 } else if (preg_match("/^ims.imscore.net.*$/",$CDR->SipRPID,$m)) {
                    $cdr['origin'] = "+31123456789";
