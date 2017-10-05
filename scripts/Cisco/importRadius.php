d2e39124d7c896cc9a8fb1b0ad2811c939ba7e7e
Tijmen de Mes <tijmen@ag-projects.com>
2017-10-05 11:52:28 +0000

    Replaced short opening tags and formatted files

--- scripts/Cisco/importRadius.php
+++ scripts/Cisco/importRadius.php
@@ -1,5 +1,5 @@
 #!/usr/bin/php
-<?
+<?php
 # This script import RADIUS records from FREERADIUS generated files
 # It accepts two arguments, first argument is the radacct file with
 # the full path, the second argument is an optional timezone against
@@ -7,7 +7,7 @@
 # (defaults to CDRTool[provider][timezone] from global.inc)
 # Example: importRadius.php /tmp/detail-20040221 Europe/Amsterdam
 
-$path=dirname(realpath($_SERVER['PHP_SELF']));
+$path = dirname(realpath($_SERVER['PHP_SELF']));
 
 require("/etc/cdrtool/global.inc");
 require("cdr_generic.php");
@@ -17,18 +17,19 @@ $CDR_class  = $DATASOURCES[$cdr_source]["class"];
 $CDRS       = new $CDR_class($cdr_source);
 
 if (checkArguments($argv)) {
-    $CDRS->import($argv[1],$argv[2]);
+    $CDRS->import($argv[1], $argv[2]);
     print "Normalizing cdrs: ";
-    $results=$CDRS->NormalizeCDRS();
+    $results = $CDRS->NormalizeCDRS();
     print "$results[total] not yet normalized calls,$results[success] normalized,$results[failure] skipped\n";
 }
 
-function checkArguments($argv) {
-    $PHP_SELF=$_SERVER['PHP_SELF'];
-	$c=count($argv);
+function checkArguments($argv)
+{
+    $PHP_SELF = $_SERVER['PHP_SELF'];
+    $c = count($argv);
     if (!count($argv) || count($argv) < 2 || count($argv) > 3) {
-    	print "Syntax: $PHP_SELF radacctFile [timezone]\n";
-		print "Example: $PHP_SELF /tmp/detail-20040221 Europe/Amsterdam\n";
+        print "Syntax: $PHP_SELF radacctFile [timezone]\n";
+        print "Example: $PHP_SELF /tmp/detail-20040221 Europe/Amsterdam\n";
         return 0;
     }
 
@@ -38,10 +39,10 @@ function checkArguments($argv) {
     }
 
     if ($argv[2]) {
-        $TZfile=$path."../../timezones";
-        $tzones=explode("\n",file_get_contents($TZfile));
-        if (!in_array($argv[2],$tzones)) {
-	        print "Error: unexisting timezone $argv[2]. See timezones file for available timezones.\n";
+        $TZfile = $path."../../timezones";
+        $tzones = explode("\n", file_get_contents($TZfile));
+        if (!in_array($argv[2], $tzones)) {
+            print "Error: unexisting timezone $argv[2]. See timezones file for available timezones.\n";
             return 0;
         }
     }
--- scripts/ratingEngine.php
+++ scripts/ratingEngine.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
-<?
-set_time_limit (0);
+<?php
+set_time_limit(0);
 ini_set('mbstring.func_overload', '0');
 ini_set('output_handler', '');
 @ob_end_flush();
@@ -11,25 +11,30 @@ require('rating.php');
 require('rating_server.php');
 
 // Init Rating Engine
-syslog(LOG_NOTICE,"Starting CDRTool Rating Engine...");
+syslog(LOG_NOTICE, "Starting CDRTool Rating Engine...");
 
 $RatingEngineServer = new RatingEngine();
 
 if (!$RatingEngineServer->init_ok) {
-    syslog(LOG_NOTICE,'Error: Cannot start Rating Engine, fix the errors and try again');
+    syslog(LOG_NOTICE, 'Error: Cannot start Rating Engine, fix the errors and try again');
     exit;
 }
 
-syslog(LOG_NOTICE,"Rating Engine started sucesfully, going to background...");
+syslog(LOG_NOTICE, "Rating Engine started sucesfully, going to background...");
 
 // Go to the background
 $d = new Daemon('/var/run/ratingEngine.pid');
 $d->start();
 
 $daemon = new socketDaemon();
-$server = $daemon->create_server('ratingEngineServer', 'ratingEngineClient', $RatingEngine['socketIP'], $RatingEngine['socketPort']);
+$server = $daemon->create_server(
+    'ratingEngineServer',
+    'ratingEngineClient',
+    $RatingEngine['socketIP'],
+    $RatingEngine['socketPort']
+);
 
-syslog(LOG_NOTICE,"Rating Engine is now ready to serve network requests");
+syslog(LOG_NOTICE, "Rating Engine is now ready to serve network requests");
 
 $daemon->process();
 
