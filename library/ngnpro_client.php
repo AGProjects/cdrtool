0c4468f46da0bf937e957d6a4ebb8382ea4a5054
Tijmen de Mes <tijmen@ag-projects.com>
2024-02-09 15:45:30 +0000

    PHP>8 fixes for undeclared variables/arrays

--- library/ngnpro_client.php
+++ library/ngnpro_client.php
@@ -356,7 +356,7 @@ class SoapEngine
 
             $default_port = 'customers';
 
-            if (count($this->allowedPorts[$this->soapEngine]) > 0) {
+            if (isset($this->allowedPorts[$this->soapEngine]) ? count($this->allowedPorts[$this->soapEngine]) : 0 > 0) {
                 if (in_array($_els[0], $this->allowedPorts[$this->soapEngine])) {
                     $this->port = $_els[0];
                 } else if (in_array($default_port, $this->allowedPorts[$this->soapEngine])) {
@@ -855,48 +855,48 @@ class Records
 
         printf("<select class=span3 name='service' onChange=\"jumpMenu('this.form')\">\n");
 
-        $j = 1;
-        foreach (array_keys($this->SoapEngine->soapEngines) as $_engine) {
-            if ($this->SoapEngine->skip[$_engine]) continue;
-            if ($j > 1) printf ("<option value=''>--------\n");
-            foreach (array_keys($this->SoapEngine->ports) as $_port) {
-                $idx = $_port.'@'.$_engine;
-                if (is_array($this->SoapEngine->skip_ports[$_engine]) && in_array($_port, $this->SoapEngine->skip_ports[$_engine])) continue;
+	$j = 1;
+	foreach (array_keys($this->SoapEngine->soapEngines) as $_engine) {
+	    if ($this->SoapEngine->skip[$_engine]) continue;
+	    if ($j > 1) printf ("<option value=''>--------\n");
+	    foreach (array_keys($this->SoapEngine->ports) as $_port) {
+		$idx = $_port.'@'.$_engine;
+		if (is_array($this->SoapEngine->skip_ports[$_engine]) && in_array($_port, $this->SoapEngine->skip_ports[$_engine])) continue;
 
-                if ($this->login_credentials['login_type'] !='admin') {
-                    if (!$pstn_access && (preg_match("/^pstn_/", $_port))) continue;
-                }
-                if (count($this->SoapEngine->allowedPorts[$_engine]) > 0 && !in_array($_port, $this->SoapEngine->allowedPorts[$_engine])) continue;
+		if ($this->login_credentials['login_type'] !='admin') {
+		    if (!$pstn_access && (preg_match("/^pstn_/", $_port))) continue;
+		}
+		if (isset($this->SoapEngine->allowedPorts[$_engine]) ? count($this->SoapEngine->allowedPorts[$_engine]) : 0 > 0 && !in_array($_port, $this->SoapEngine->allowedPorts[$_engine])) continue;
 
-                // disable some version dependent ports
+		// disable some version dependent ports
 
-                if ($_port == 'customers' && $this->SoapEngine->soapEngines[$_engine]['version'] < 2) continue;
+		if ($_port == 'customers' && $this->SoapEngine->soapEngines[$_engine]['version'] < 2) continue;
 
-                if ($this->SoapEngine->ports[$_port]['resellers_only']) {
-                    if ($this->login_credentials['login_type']=='admin' || $this->loginAccount->resellerActive) {
-                        printf(
-                            "<option value=\"%s@%s\"%s>%s@%s\n",
-                            $_port,
-                            $_engine,
-                            $selected_soapEngine[$idx],
-                            $this->SoapEngine->ports[$_port]['name'],
-                            $this->SoapEngine->soapEngines[$_engine]['name']
-                        );
-                    }
-                } else {
-                    printf(
-                        "<option value=\"%s@%s\"%s>%s@%s\n",
-                        $_port,
-                        $_engine,
-                        $selected_soapEngine[$idx],
-                        $this->SoapEngine->ports[$_port]['name'],
-                        $this->SoapEngine->soapEngines[$_engine]['name']
-                    );
-                }
-            }
+		if ($this->SoapEngine->ports[$_port]['resellers_only']) {
+		    if ($this->login_credentials['login_type']=='admin' || $this->loginAccount->resellerActive) {
+			printf(
+			    "<option value=\"%s@%s\"%s>%s@%s\n",
+			    $_port,
+			    $_engine,
+			    $selected_soapEngine[$idx],
+			    $this->SoapEngine->ports[$_port]['name'],
+			    $this->SoapEngine->soapEngines[$_engine]['name']
+			);
+		    }
+		} else {
+		    printf(
+			"<option value=\"%s@%s\"%s>%s@%s\n",
+			$_port,
+			$_engine,
+			$selected_soapEngine[$idx],
+			$this->SoapEngine->ports[$_port]['name'],
+			$this->SoapEngine->soapEngines[$_engine]['name']
+		    );
+		}
+	    }
 
-            $j++;
-        }
+	    $j++;
+	}
         print("</select>");
         printf(
             "<script type=\"text/JavaScript\">
@@ -1121,14 +1121,16 @@ class Records
             printf("<input type=hidden name=adminonly value='%s'>", $this->adminonly);
         }
 
-        foreach (array_keys($this->SoapEngine->extraFormElements) as $element) {
-            if (!strlen($this->SoapEngine->extraFormElements[$element])) continue;
-            printf(
-                "<input type=hidden name=%s value='%s'>\n",
-                $element,
-                $this->SoapEngine->extraFormElements[$element]
-            );
-        }
+        if (is_array($this->SoapEngine->extraFormElements)) {
+	    foreach (array_keys($this->SoapEngine->extraFormElements) as $element) {
+		if (!strlen($this->SoapEngine->extraFormElements[$element])) continue;
+		printf(
+		    "<input type=hidden name=%s value='%s'>\n",
+		    $element,
+		    $this->SoapEngine->extraFormElements[$element]
+		);
+	    }
+	}
     }
 
     function getAllowedDomains()
@@ -1750,7 +1752,9 @@ class Records
 
     function getCustomerProperty($name = '')
     {
-        if (!count($this->loginProperties)) return false;
+	if (empty($this->loginProperties)) {
+	    return false;
+	}
 
         foreach ($this->loginProperties as $_property) {
             if ($_property->name == $name) {
@@ -1763,7 +1767,9 @@ class Records
 
     function getResellerProperty($name = '')
     {
-        if (!count($this->resellerProperties)) return false;
+	if (empty($this->resellerProperties)) {
+	    return false;
+	}
 
         foreach ($this->resellerProperties as $_property) {
             if ($_property->name == $name) {
