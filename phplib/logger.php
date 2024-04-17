1dc1825f69a65e79e3abe867a485bc92ec6946f9
Tijmen de Mes <tijmen@ag-projects.com>
2024-04-17 11:37:41 +0000

    Added browser logger

--- phplib/logger.php
+++ phplib/logger.php
@@ -6,6 +6,7 @@ use Monolog\Logger;
 use Monolog\Handler\SyslogHandler;
 use Monolog\Handler\BrowserConsoleHandler;
 use Monolog\Formatter\LineFormatter;
+use Monolog\Processor\WebProcessor;
 
 global $logger;
 
@@ -14,6 +15,7 @@ $syslog = new SyslogHandler('cdrtool', 'local0');
 $formatter = new LineFormatter("%channel%: %message% %extra%");
 $syslog->setFormatter($formatter);
 $logger->pushHandler($syslog);
+$logger->pushProcessor(new WebProcessor(null, ['ip']));
 
 global $browserLogger;
 $browserLogger = new Logger('CDRTool');
@@ -25,6 +27,10 @@ function logger($message, $level = 'notice')
     global $logger;
     if ($level == 'notice') {
         notice($message);
+    } elseif ($level == 'error') {
+	error($message);
+    } elseif ($level == 'critical') {
+	critical($message);
     }
 }
 
@@ -32,4 +38,16 @@ function notice($message)
 {
     global $logger;
     $logger->notice($message);
+}
+
+function error($message)
+{
+    global $logger;
+    $logger->error($message);
+}
+
+function critical($message)
+{
+    global $logger;
+    $logger->critical($message);
 }
