68235b61961dcea30a0a9a08fece8f4e3af4e8d6
Tijmen de Mes <tijmen@ag-projects.com>
2024-05-08 14:31:13 +0000

    Update crontab

--- debian/cron.d
+++ debian/cron.d
@@ -1,23 +1,23 @@
 # Normalization
-*/5 * * * * root test /var/www/CDRTool/scripts/normalize.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/normalize.php  >/dev/null
+*/5 * * * * root [ -x /var/www/CDRTool/scripts/normalize.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/normalize.php  >/dev/null ;fi
 
 # Check quota
-*/5 * * * * root  test /var/www/CDRTool/scripts/OpenSIPS/quotaCheck.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/OpenSIPS/quotaCheck.php    >/dev/null
-  0 2 1 * * root  test /var/www/CDRTool/scripts/OpenSIPS/quotaReset.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/OpenSIPS/quotaReset.php    >/dev/null
- 10 0 * * * root  test /var/www/CDRTool/scripts/OpenSIPS/quotaDailyReset.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/OpenSIPS/quotaDailyReset.php >/dev/null
+*/5 * * * * root [ -x /var/www/CDRTool/scripts/OpenSIPS/quotaCheck.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/OpenSIPS/quotaCheck.php >/dev/null; fi
+  0 2 1 * * root [ -x  /var/www/CDRTool/scripts/OpenSIPS/quotaReset.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/OpenSIPS/quotaReset.php    >/dev/null ; fi
+ 10 0 * * * root [ -x  /var/www/CDRTool/scripts/OpenSIPS/quotaDailyReset.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/OpenSIPS/quotaDailyReset.php >/dev/null ; fi
  
 # Purge SIP trace table
- 20 3 * * * root  test /var/www/CDRTool/scripts/OpenSIPS/purgeSIPTrace.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/OpenSIPS/purgeSIPTrace.php >/dev/null
+ 20 3 * * * root [ -x  /var/www/CDRTool/scripts/OpenSIPS/purgeSIPTrace.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/OpenSIPS/purgeSIPTrace.php >/dev/null ; fi
 
 # Statistics
-*/5  * * * * root  test /var/www/CDRTool/scripts/buildStatistics.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/buildStatistics.php >/dev/null
+*/5  * * * * root [ -x  /var/www/CDRTool/scripts/buildStatistics.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/buildStatistics.php >/dev/null ;fi
 
 # Next two jobs are only used when using a central radacct table:
-#  0  3 1 * * root test /var/www/CDRTool/scripts/OpenSIPS/rotateTables.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/OpenSIPS/rotateTables.php >/dev/null
-#  0  4 * * * root test /var/www/CDRTool/scripts/purgeTables.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/purgeTables.php >/dev/null
+#  0  3 1 * * root [ -x  /var/www/CDRTool/scripts/OpenSIPS/rotateTables.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/OpenSIPS/rotateTables.php >/dev/null ; fi
+#  0  4 * * * root [ -x  /var/www/CDRTool/scripts/purgeTables.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/purgeTables.php >/dev/null; fi
 
 # Send email with last missed calls to SIP subscribers
-15 2 * * * root  test /var/www/CDRTool/scripts/notifyLastSessions.php -a \! -d /run/systemd/system && /var/www/CDRTool/scripts/OpenSIPS/notifyLastSessions.php >/dev/null
+15 2 * * * root [ -x  /var/www/CDRTool/scripts/notifyLastSessions.php ] && if [ ! -d /run/systemd/system ]; then /var/www/CDRTool/scripts/OpenSIPS/notifyLastSessions.php >/dev/null ; fi
 
 # Import rating tables
-15 5 * * * root  test /var/www/CDRTool/scripts/importRatingTables.php -a \! -d /run/systemd/system &&  /var/www/CDRTool/scripts/importRatingTables.php >/dev/null
+15 5 * * * root [ -x  /var/www/CDRTool/scripts/importRatingTables.php ] && if [ ! -d /run/systemd/system ]; then  /var/www/CDRTool/scripts/importRatingTables.php >/dev/null ;fi
