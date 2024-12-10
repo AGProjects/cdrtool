a367d513aa57df62d760b3e59cefb72a5b1abdf0
Tijmen de Mes <tijmen@ag-projects.com>
2024-12-10 10:40:01 +0000

    Updated layouyt in media sessions

--- library/media_sessions.php
+++ library/media_sessions.php
@@ -449,7 +449,7 @@ class MediaSessions
                         <div><nobr><b>From:</b> %s</nobr></div>
                         <div style="flex: 1 1 auto; margin-left:5px; margin-right: 5px">
                             <div class="sarrow right" style="display: inline-block;">
-                                <span style="position: absolute; transform: translate(-50%%, -50%%); top: 50%%; left: 50%%; font-family: monospace, courier; font-size: 8pt;" class="label label-inverse">%s</span>
+                                <span style="position: absolute; transform: translate(0, -50%%); left: 24px; font-family: monospace, courier; font-size: 8pt; text-shadow: none" class="label">%s</span>
                             </div>
                         </div>
                         <div><nobr><b>To:</b> %s</nobr</div>
